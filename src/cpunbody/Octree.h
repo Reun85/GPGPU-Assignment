65d0a1a9514006020224f0132abdfa7015d46228
Reun85 <gugigergo@gmail.com>
2024-05-05 15:08:20 +0200

    Added a function for Depth First Travelsal of tree

--- src/cpunbody/Octree.h
+++ src/cpunbody/Octree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
-#include <chrono>
+#include <functional>
+#include <memory>
 #include <ostream>
 #include <vector>
 
@@ -57,6 +58,21 @@ class Node {
   bool m_is_leaf = true;
 };
 
+/// Default is bool to have as little size as possible.
+// C++ does not have empty, zero sized types.
+template <typename T = bool>
+class NodeWithExtraInfo {
+ public:
+  Node *inner;
+  size_t depth;
+  glm::vec3 center;
+  glm::vec3 size;
+  std::shared_ptr<NodeWithExtraInfo> parent = nullptr;
+  /// Can be anything
+  /// Must have a default constructor
+  T user;
+};
+
 class Octree {
  public:
   // The default times it is cut before injecting elements
@@ -66,6 +82,13 @@ class Octree {
   Octree(std::vector<Particle> &particles);
   void Recalculate(std::vector<Particle> &particles);
 
+ public:
+  /// T default is bool to have as little size as possible.
+  // C++ does not have empty, zero sized types.
+  template <typename T = bool, bool onlyOnLeaves = false>
+  void DepthFirstTravel(
+      std::function<void(std::shared_ptr<NodeWithExtraInfo<T>>)> fun) const;
+
  private:
   std::vector<Node> m_nodes;
   glm::vec3 m_size;
@@ -74,3 +97,37 @@ class Octree {
   size_t itr_start;
   friend std::ostream &operator<<(std::ostream &os, const Octree &octree);
 };
+
+template <typename T, bool onlyOnLeaves>
+void Octree::DepthFirstTravel(
+    std::function<void(std::shared_ptr<NodeWithExtraInfo<T>>)> fun) const {
+  /// Set sizes for Octree Nodes
+  using MS = std::shared_ptr<NodeWithExtraInfo<T>>;
+  std::vector<std::shared_ptr<NodeWithExtraInfo<T>>> stack;
+  glm::vec3 allsize = m_size;
+  stack.emplace_back(std::make_shared<NodeWithExtraInfo<T>>(
+      NodeWithExtraInfo<T>{m_root, 0, m_center, 0.5f * allsize}));
+
+  while (!stack.empty()) {
+    MS current = stack.back();
+    stack.pop_back();
+    if (!current->inner->m_is_leaf) {
+      glm::vec3 currsize = 0.5f * current->size;
+      for (int i = 0; i < 8; i++) {
+        glm::vec3 currcenter = current->center + (currsize * Node::OFFSETS[i]);
+
+        stack.emplace_back(
+            std::make_shared<NodeWithExtraInfo<T>>(NodeWithExtraInfo<T>{
+                current->inner->m_children[i], current->depth + 1, currcenter,
+                currsize, MS(current)}));
+      }
+    } else {
+      if (onlyOnLeaves) {
+        if (current->inner->m_is_leaf) {
+          fun(current);
+        }
+      } else
+        fun(current);
+    }
+  }
+}
--- src/cpunbody/Octreostream.cpp
+++ src/cpunbody/Octreostream.cpp
@@ -1,39 +1,33 @@
-
-#include <memory>
-
 #include "Octree.h"
 
-struct Depth {
-  size_t depth;
-  Depth(const size_t depth) : depth(depth){};
+struct InsertTabs {
+  size_t count;
+  InsertTabs(const size_t count) : count(count){};
 };
 
-std::ostream &operator<<(std::ostream &os, const Depth &depth) {
-  for (int i = 0; i < depth.depth; i++) os << "\t";
+std::ostream &operator<<(std::ostream &os, const InsertTabs &count) {
+  for (size_t i = 0; i < count.count; i++) os << "\t";
   return os;
 }
 
-struct Stuff {
-  Node *node;
-  size_t depth;
-  glm::vec3 center;
-  glm::vec3 size;
+struct UserInfo {
   bool printed = false;
-  std::shared_ptr<Stuff> prev = nullptr;
 };
-std::ostream &operator<<(std::ostream &os, Stuff *current) {
-  if (current->printed) return os;
+std::ostream &operator<<(std::ostream &os,
+                         NodeWithExtraInfo<UserInfo> *current) {
+  if (current->user.printed) return os;
 
-  if (current->prev != nullptr) {
-    os << current->prev;
+  if (current->parent != nullptr) {
+    os << current->parent;
   }
-  os << Depth(current->depth) << "Node:"
+  os << InsertTabs(current->depth) << "Node:"
      << "\n"
-     << Depth(current->depth) << "center: " << Vec3<float>(current->center)
+     << InsertTabs(current->depth) << "center: " << Vec3<float>(current->center)
      << "\n"
-     << Depth(current->depth) << "size: " << Vec3<float>(current->size) << "\n"
-     << Depth(current->depth) << "children: " << std::endl;
-  current->printed = true;
+     << InsertTabs(current->depth) << "size: " << Vec3<float>(current->size)
+     << "\n"
+     << InsertTabs(current->depth) << "children: " << std::endl;
+  current->user.printed = true;
 
   return os;
 }
@@ -42,39 +36,15 @@ std::ostream &operator<<(std::ostream &os, const Octree &octree) {
 
   os << "Octree: \n{\n";
 
-  using MS = std::shared_ptr<Stuff>;
-  std::vector<std::shared_ptr<Stuff>> stack;
-  glm::vec3 allsize = octree.m_size;
-  stack.emplace_back(std::make_shared<Stuff>(
-      Stuff{octree.m_root, 0, octree.m_center, 0.5f * allsize, true}));
-
-  while (!stack.empty()) {
-    MS current = stack.back();
-    stack.pop_back();
-    // if (current->depth == Octree::START_DEPTH) {
-    //   os << current.get();
-    // }
-    // if (current->depth == Octree::START_DEPTH) {
-    //   os << current.get();
-    // }
-    if (!current->node->m_is_leaf) {
-      glm::vec3 currsize = 0.5f * current->size;
-      for (int i = 0; i < 8; i++) {
-        glm::vec3 currcenter = current->center + (currsize * Node::OFFSETS[i]);
-
-        stack.emplace_back(std::make_shared<Stuff>(
-            Stuff{current->node->m_children[i], current->depth + 1, currcenter,
-                  currsize, false, MS(current)}));
-      }
-    } else {
-      if (current->node->m_particle != nullptr) {
-        os << current;
-        os << Depth(current->depth + 1)
-           << "Particle: " << Vec3<float>(current->node->m_particle->m_position)
-           << std::endl;
-      }
-    }
-  }
+  octree.DepthFirstTravel<UserInfo>(
+      [&os](std::shared_ptr<NodeWithExtraInfo<UserInfo>> current) {
+        if (current->inner->m_particle != nullptr) {
+          os << current;
+          os << InsertTabs(current->depth + 1) << "Particle: "
+             << Vec3<float>(current->inner->m_particle->m_position)
+             << std::endl;
+        }
+      });
 
   os << "}" << std::endl;
   os << "center" << Vec3<float>(octree.m_center) << std::endl;
